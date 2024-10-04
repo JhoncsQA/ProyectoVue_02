642d01d999734006ad419ba84a341ec2fae02dcd
JhoncsQA <jhoncabezas@unicomfacauca.edu.co>
2024-10-03 21:45:35 -0500

    Desarrollo modulr

--- proyectoVueJhon/src/App.vue
+++ proyectoVueJhon/src/App.vue
@@ -1,35 +1,12 @@
 <template>
 
     <h1>Hola Mundo</h1>
-    <h3>Contador: {{ contador }}</h3>
-    <h3>Cuadrado: {{ contador*contador }}</h3>
-
-    <div>
-        <button v-on:click="ChangeSumar">+1</button>
-        <button v-on:click="ChangeRestar">-1</button>
-    </div>
+    <Mycounter :value="5"/>
 
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-const contador=ref(100);
-
-const ChangeSumar=()=>{
-    contador.value=contador.value+1;
-}
-const ChangeRestar=()=>{
-    contador.value=contador.value-1;
-
-}
-
-console.log("Hola Mundo");
+import Mycounter from './Components/MiCounter.vue'
 
 </script>
 
-<style>
-    h1{
-        color: brown;
-    }
-
-</style>
---
+++ proyectoVueJhon/src/Components/MiCounter.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <h1>Hola Mundo</h1>
+    <h3>Contador: {{ contador }}</h3>
+    <h3>Cuadrado: {{ contador*contador }}</h3>
+
+    <div>
+        <button @click="contador++">+1</button>
+        <button @click="contador--">-1</button>
+    </div>
+
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    value:{type:Number, required:true},
+
+})
+const contador =ref(props.value);
+
+console.log("Hola Mundo");
+
+</script>
+
